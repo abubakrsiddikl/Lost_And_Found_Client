b9d4f1474001c873ea8ff45e20a1ee218c2d5f1f
limon <limonsk026@gmail.com>
2024-12-24 15:52:37 +0600

    banner section complete

---
+++ src/components/pages/Home/Banner.jsx
@@ -0,0 +1,43 @@
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+// import required modules
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
+import slider1 from "../../../assets/slider1.jpg";
+import slider2 from "../../../assets/slider2.jpg";
+import slider3 from "../../../assets/slider3.jpeg";
+
+
+const Banner = () => {
+  return (
+    <div className="w-11/12 mx-auto mt-5">
+      <Swiper
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide><img src={slider1} className="w-full h-[300px] md:h-[450px]" alt="" /></SwiperSlide>
+        <SwiperSlide><img src={slider2} className="w-full h-[300px] md:h-[450px]" alt="" /></SwiperSlide>
+        <SwiperSlide><img src={slider3} className="w-full h-[300px] md:h-[450px]" alt="" /></SwiperSlide>
+        
+      </Swiper>
+    </div>
+  );
+};
+
+export default Banner;
